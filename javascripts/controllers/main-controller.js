4558c17a904464acb4374b1dece42ab598a066d1
gabriel <ariel_rando@hotmail.com.ar>
2018-08-27 23:37:25 -0300

    estructura - inicio

--- javascripts/controllers/main-controller.js
+++ javascripts/controllers/main-controller.js
@@ -1,12 +1,12 @@
 'use strict';
 angular.module('abogadoApp')
     .controller('mainController', function($auth, $scope, $rootScope, $state, $filter, $log, $http, $translate, config) {
-        console.log(":D");
+
     })
     .controller('seccion1', function($auth, $scope, $rootScope, $state, $filter, $log, $http, $translate, config) {
-        console.log(":(");
+
     })
 
     .controller('contactoController', function($auth, $scope, $rootScope, $state, $filter, $log, $http, $translate, config) {
-      console.log(":(");
+      
     });
--- javascripts/routers/abogado-router.js
+++ javascripts/routers/abogado-router.js
@@ -1,22 +1,22 @@
 'use strict';
 angular.module('abogadoRouter', []).config(function($stateProvider) {
-    $stateProvider
-        .state('main', {
-            cache: false,
-            url: '/', // url que va arriba
-            templateUrl: 'templates/views/main/index.html', //ubicacion del html
-            controller: 'mainController' //nombre controller
-        })
-        .state('seccion1', {
-            cache: false,
-            url: '/seccion1', // url que va arriba
-            templateUrl: 'templates/views/main/seccion1.html', //ubicacion del html
-            controller: 'seccion1' //nombre controller
-        })
-        .state('contacto', {
-            cache: false,
-            url: '/contacto', // url que va arriba
-            templateUrl: 'templates/views/main/contacto.html', //ubicacion del html
-            controller: 'contactoController' //nombre controller
-        });
+  $stateProvider
+    .state('main', {
+      cache: false,
+      url: '/', // url que va arriba
+      templateUrl: 'templates/views/main/index.html', //ubicacion del html
+      controller: 'mainController' //nombre controller
+    })
+    .state('seccion1', {
+      cache: false,
+      url: '/seccion1', // url que va arriba
+      templateUrl: 'templates/views/main/seccion1.html', //ubicacion del html
+      controller: 'seccion1' //nombre controller
+    })
+    .state('contacto', {
+      cache: false,
+      url: '/contacto', // url que va arriba
+      templateUrl: 'templates/views/main/contacto.html', //ubicacion del html
+      controller: 'contactoController' //nombre controller
+    });
 });
